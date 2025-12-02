6aa7003ea8ed0677d84ff9a82f1326f778c9ee92
TheBigoranger <ethanxuyicheng@gmail.com>
2025-12-01 21:45:59 -0800

    build: auto deploy 2025-12-01 21:45:59

--- deploy_astro.sh
+++ deploy_astro.sh
@@ -2,5 +2,5 @@
 npx astro build
 git add .
 git commit -m "build: auto deploy $(date '+%Y-%m-%d %H:%M:%S')" || true
-git remote set-url origin git@github.com:thebigoranger/Portfolio.git
+#git remote set-url origin git@github.com:thebigoranger/Portfolio.git
 git push
