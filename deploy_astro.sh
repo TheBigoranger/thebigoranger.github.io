08fb0b767cca0e2978b59d7ad64cdeb7abab371a
TheBigoranger <ethanxuyicheng@gmail.com>
2025-12-16 16:21:24 -0800

    build: auto deploy 2025-12-16 16:21:24

--- deploy_astro.sh
+++ deploy_astro.sh
@@ -4,3 +4,6 @@ git add .
 git commit -m "build: auto deploy $(date '+%Y-%m-%d %H:%M:%S')" || true
 #git remote set-url origin git@github.com:thebigoranger/Portfolio.git
 git push
+# compile the CV pdf again
+cd /var/www/CV-Yicheng-Xu
+sh git_push.sh
